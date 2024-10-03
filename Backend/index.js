156207f8022daf4cdab887dfb20426d0737f52f8
DhavalBavda <dhavalbavdas@gmail.com>
2024-10-03 11:39:43 +0530

    ..

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "*"
 }));
 
 app.use(express.static(path.join(__dirname, "public")));
